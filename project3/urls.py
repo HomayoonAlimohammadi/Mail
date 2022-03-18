b7795935b733601b780d046507e748fc4df31598
Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>
2022-03-18 10:43:44 +0330

    Added main_app:index

--- project3/urls.py
+++ project3/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from mail.views import index_view
+from .views import index_view
 
 
 app_name = 'main'
